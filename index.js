1568ed1d7b2211bc0d5e30066455ff3a6d41a9de
dexia2 <lindan.k2012.af.nk@gmail.com>
2018-02-19 11:25:03 +0900

    output csv

--- index.js
+++ index.js
@@ -1,18 +1,30 @@
 const fs = require('fs');
+
+// json読み込み
 const json = JSON.parse(fs.readFileSync('./ws.json', 'utf8'));
 const rallies = json.details[0].rallies;
+const csv = require('csv');
 
+// ストロークの吐き出し
 const flatten = arr => arr.reduce((a, e) => a.concat(...e), []);
+const toStrokesStats = strokes => {
+    let index = 1;
+    return strokes.map(s => Object.assign({}, s, {index: index++}))};
+const allStrokes = rallies => flatten(rallies.map(r => toStrokesStats(r.strokes)));
 
-const allStrokes = rallies => flatten(rallies.map(r => r.strokes));
+csv.stringify(allStrokes(rallies), { header: true}, function(err, output){
+    fs.writeFileSync('strokes.csv', output, 'utf8');
+});
 
-const allHighback = (rallies) => {
-    return allStrokes(rallies).filter(s => s.from === 'back-left' && s.hand === 'backhand');
-};
-
-const allRound = (rallies) => {
-    return allStrokes(rallies).filter(s => s.from === 'back-left');
-};
-
-
-console.log(`${allHighback(rallies).length} /${allRound(rallies).length}`);
+// ラリーの吐き出し
+const toRallyStats = rallies => rallies.map(r =>
+                                            ({
+                                                wonby: r.wonby,
+                                                unforced: r.unforced,
+                                                cause: r.cause,
+                                                strokesCount: r.strokes.length,
+                                                lastStroke: r.strokes[r.strokes.length -1]
+                                            }));
+csv.stringify(toRallyStats(rallies), { header: true}, function(err, output){
+    fs.writeFileSync('rallies.csv', output, 'utf8');
+});
